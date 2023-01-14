f1adcdc6a7f67d162ebb74af426c464186a910e5
andres06-hub <andressierrarojas7@gmail.com>
2023-01-14 03:08:58 -0500

    ADD: routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+  import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'posts', loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule)}
+  {path: '', redirectTo: 'posts', pathMatch: 'full'},
+  {path: 'posts', loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule)},
+  {path: 'not-found', loadChildren: () => import('./modules/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
+
 ];
 
 @NgModule({
