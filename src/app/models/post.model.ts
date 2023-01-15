6cf1bd7cedf3a81bb3c6fc987aa43a2bcba97b5f
andres06-hub <andressierrarojas7@gmail.com>
2023-01-15 00:33:36 -0500

    ADD: modified and interface added

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,7 +1,9 @@
+import { Author } from './author.model';
+
 export interface Post {
   id: number;
   title: string;
   content: string;
   authorId: number;
-  authorName: string;
+  author: Author;
 }
