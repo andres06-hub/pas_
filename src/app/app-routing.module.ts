2f3c8938d88b1be3b106893f4e7196800a943ba5
andres06-hub <andressierrarojas7@gmail.com>
2023-01-17 03:32:43 -0500

    NEW: Home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'posts', pathMatch: 'full'},
+  {path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
   {path: 'posts', loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule)},
   {path: 'not-found', loadChildren: () => import('./modules/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
