9ec765e87eb1181da38e37a6fcc9fcbb6be8282d
andres06-hub <andressierrarojas7@gmail.com>
2023-01-19 22:15:09 -0500

    DELETED

--- src/app/models/author.model.ts
+++
@@ -1,7 +0,0 @@
-import { Post } from "./post.model"
-
-export interface Author {
-  id: number,
-  name: string,
-  posts?: Post[]
-}
--- src/app/models/post.model.ts
+++
@@ -1,9 +0,0 @@
-import { Author } from './author.model';
-
-export interface Post {
-  id: number;
-  title: string;
-  content: string;
-  authorId: number;
-  author: Author;
-}
