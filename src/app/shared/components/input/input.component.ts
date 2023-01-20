2669dd21755e41477d2f9fd4dc580db9d5c9ea40
andres06-hub <andressierrarojas7@gmail.com>
2023-01-19 21:58:44 -0500

    NEW: component

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent {
+
+  @Input() public label: string = '';
+  @Input() public hideLabel: boolean = false;
+  @Input() public type: string = '';
+  @Input() public placeholder: string = '';
+  @Input() public content: string = '';
+}
