4d53ba227f85b5d5ce0891e848815872048d2270
andres06-hub <andressierrarojas7@gmail.com>
2023-01-20 01:50:59 -0500

    NEW: Style and added a icon

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule
+    SharedModule,
+    FontAwesomeModule
   ]
 })
 export class HomeModule { }
