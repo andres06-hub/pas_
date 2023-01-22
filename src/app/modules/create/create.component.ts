74b247d9dd8f54f13f4b7b7c77259614b478a123
andres06-hub <andressierrarojas7@gmail.com>
2023-01-21 19:57:17 -0500

    ADD: The process of obtaining the data from the form is started

--- src/app/modules/create/create.component.ts
+++ src/app/modules/create/create.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, ViewChildren, QueryList } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Type } from 'src/app/models/enums/input.type';
+import { InputComponent } from 'src/app/shared/components/input/input.component';
 
 @Component({
   selector: 'app-create',
@@ -10,36 +11,35 @@ import { Type } from 'src/app/models/enums/input.type';
 export class CreateComponent {
 
   constructor(private fb: FormBuilder) {}
-  //POST
+  // public form: FormGroup = new FormGroup({});
+
   postT: string = 'Create Post';
-  type = {
-    txt: Type.Text,
-    num: Type.Num,
-  }
-  label = {
-    np1: 'Title',
-    np2: 'Content',
-    np3: 'IdAuthor',
-    np4: 'Name'
-  };
-  placeHolder = {
-    title: 'Title',
-    content: 'Content',
-    idAuthor: 'Id Author',
-    name: 'Name',
-  };
-  //Author
   authorT: string = 'Create Author';
 
-  public form: FormGroup = this.fb.group({
-    title : ['', Validators.compose([Validators.required])],
-    content : ['', Validators.compose([Validators.required])],
-    idAuthor : ['', Validators.compose([Validators.required])],
-  });
+  inputRefs: { [key: string]: InputComponent } = {}
 
+  @ViewChildren(InputComponent) public set formP(value: QueryList<InputComponent>) {
+    value.forEach((el: InputComponent) => {
+      const name = el.ngControl.name;
+      if (!name || name in this.inputRefs) return;
+      this.inputRefs[name] = el;
+      console.log(el);
+
+    });
+  }
+
+  public form: FormGroup = this.fb.group({
+    // title : new FormControl('', [Validators.required]),
+    // content : new FormControl('', [Validators.required]),
+    // idAuthor : new FormControl('', [Validators.required]),
+    title: ['', Validators.compose([Validators.required])],
+    content: ['', Validators.compose([Validators.required])],
+    idAuthor: ['', Validators.compose([Validators.required])],
+  });
 
   onFormSubmit() {
     const { title, content, idAuthor } = this.form.value;
+    console.warn(this.form.value);
     console.warn({title,content,idAuthor});
   }
 }
